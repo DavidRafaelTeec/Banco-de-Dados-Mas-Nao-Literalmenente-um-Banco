c5ece3d2663d4d995733251b27ca69248dc6d391
David Rafael <69859496+DavidRafaelTeec@users.noreply.github.com>
2020-08-24 15:25:24 -0300

    Add files via upload
    Banco de dados primeiro script

---
+++ aula01.sql
@@ -0,0 +1,12 @@
+create database Aluno;
+use Aluno;
+
+-- criando tabela
+
+Create Table Aluno (
+	cd_ra int primary key,
+	nm_aluno varchar(50),
+    ds_bairro varchar(30)
+);
+
+Select * from aluno; 
